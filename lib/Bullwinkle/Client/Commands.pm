bcd969ba61ccb83e40d758a34ec32e436cb46c37
bowtie <bowtie@cpan.org>
2012-10-15 02:04:44 +0100

    POD tweaks

--- lib/Bullwinkle/Client/Commands.pm
+++ lib/Bullwinkle/Client/Commands.pm
@@ -91,7 +91,7 @@ Bullwinkle::Client::Commands
 
 =head1 VERSION
 
-version  0.01_01
+version  0.01_04
 
 =head1 DESCRIPTION
 
@@ -102,13 +102,13 @@ Provides the Message blocks for the Bullwinkle Test Client
 
 =over 4
 
-=item *	continue_null
-
 =item *	continue_file
 
 =item *	continue_function
 
 =item *	continue_line
+
+=item *	continue_null
 
 =item *	quit
 
@@ -128,19 +128,28 @@ Moo
 
 Bullwinkle::Server::Response
 
+
 =head1 AUTHOR
 
 Kevin Dawson E<lt>bowtie@cpan.orgE<gt>
 
 Rocky Bernstein E<lt>rocky@cpan.orgE<gt>
 
-=head1 LICENCE AND COPYRIGHT
 
-Copyright &copy 2012, Kevin Dawson E<lt>bowtie@cpan.orgE<gt> Rocky Bernstein E<lt>rocky@cpan.orgE<gt>. All rights reserved.
+=head2 CONTRIBUTORS
 
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
+=head1 COPYRIGHT
+ 
+Copyright E<copy> 2012, the Bullwinkle L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+ 
+This library is free software and may be distributed under the same terms
+as perl itself.
+
 
 =cut
+
 
--- lib/Bullwinkle/Server/Response.pm
+++ lib/Bullwinkle/Server/Response.pm
@@ -99,7 +99,7 @@ Bullwinkle::Server::Response.
 
 =head1 VERSION
 
-version  0.01_01
+version  0.01_04
 
 =head1 DESCRIPTION
 
@@ -143,12 +143,20 @@ Kevin Dawson E<lt>bowtie@cpan.orgE<gt>
 
 Rocky Bernstein E<lt>rocky@cpan.orgE<gt>
 
-=head1 LICENCE AND COPYRIGHT
 
-Copyright &copy 2012, Kevin Dawson E<lt>bowtie@cpan.orgE<gt> Rocky Bernstein E<lt>rocky@cpan.orgE<gt>. All rights reserved.
+=head2 CONTRIBUTORS
 
 
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
+=head1 COPYRIGHT
+ 
+Copyright E<copy> 2012, the Bullwinkle L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+ 
+This library is free software and may be distributed under the same terms
+as perl itself.
+
 
 =cut
+
